d8e48056d55a24926ab22552944223877eb6b414
“A1-4U2T1NN” <cagraham224@gmail.com>
2025-07-14 10:35:00 -0400

    feat: add Not found page when user is linked to a page that does not exist;

--- my-app/src/pages/NotFound.tsx
+++ my-app/src/pages/NotFound.tsx
@@ -1,12 +1,27 @@
 import React from "react";
-
+import NotFoundImage from "../assets/404.png";
 function NotFound() {
   return (
-    <>
-      <div>
-        <h1 className="text-3xl font-bold underline">Page Not Found</h1>
-      </div>
-    </>
+<div className="flex flex-col items-center justify-start h-screen text-center px-4 pt-30">
+  <img
+    src={NotFoundImage}
+    alt="404 Not Found"
+    className="max-w-xs"
+  />
+  <h1 className="text-4xl font-extrabold mb-2 text-gray-800">
+    Oops... Page Not Found
+  </h1>
+  <p className="text-gray-600 mb-4 max-w-md">
+    The page you're looking for doesn't exist. If you're lost, click below
+    to return to the home page.
+  </p>
+  <a
+    href="/"
+    className="bg-[#915f6e] text-white px-6 py-2 rounded-md hover:bg-[#7f4f5b] transition"
+  >
+    Return Home
+  </a>
+</div>
   );
 }
 
